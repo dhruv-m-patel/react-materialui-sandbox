f79d0054a2456add67dc201b4e2db170c7db337f
Dhruv Patel <dhruv-m-patel@users.noreply.github.com>
2021-03-10 15:26:25 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import "./styles.css";
+import React from 'react'
+import { makeStyles, CssBaseline } from '@material-ui/core'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import clsx from 'clsx'
+
+const useStyles = makeStyles(() => ({
+    container: {
+        margin: 0,
+    },
+    code: {
+        fontFamily: 'source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace',
+    },
+    app: {
+        textAlign: 'center',
+    },
+    appLogo: {
+        height: '40vmin',
+        pointerEvents: 'none',
+    },
+    appHeader: {
+        backgroundColor: '#282c34',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white',
+    },
+    appLink: {
+        color: '#61dafb',
+    },
+}))
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    const classes = useStyles()
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Box className={clsx(classes.container, classes.app)}>
+                <Typography variant="h1" className={classes.appHeader}>
+                    Welcome to react-materialui-sandbox
+                </Typography>
+            </Box>
+        </React.Fragment>
+    )
 }
