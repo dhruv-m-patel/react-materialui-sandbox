3bab1c3e57377fc88b3c9148e3016248c9be8022
Dhruv Patel <dhruv-m-patel@users.noreply.github.com>
2021-03-10 16:10:06 -0600

    initial commit (#2)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import clsx from 'clsx'
+
+const useStyles = makeStyles(() => ({
+    app: {
+        fontFamily: `'Roboto', Arial, Helvetica, sans-serif`,
+        fontSize: '1rem',
+    },
+}))
+
+export default function App() {
+    const classes = useStyles()
+
+    return (
+        <Box className={clsx(classes.app)}>
+            <Typography variant="h5">Welcome to react-materialui-sandbox</Typography>
+        </Box>
+    )
+}
