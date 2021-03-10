ad7d88ddfe3ead9e2d6b2849b94f2ecc9852f2e9
Dhruv Patel <dhruv-m-patel@users.noreply.github.com>
2021-03-10 16:09:41 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-
+import { CssBaseline } from '@material-ui/core'
 import App from './App'
 
-const rootElement = document.getElementById('root')
 ReactDOM.render(
     <StrictMode>
+        <CssBaseline />
         <App />
     </StrictMode>,
-    rootElement
+    document.getElementById('root')
 )
